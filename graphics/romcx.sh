65f1d73476f127567317df3e8698caa6c736dab7
kannagi <kannagichan@gmail.com>
2016-04-13 18:52:34 +0200

    update script and sprite

---
+++ graphics/romcx.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+neogeoconvert tile1.png +0 -o hyprball-c1.c1
+neogeoconvert tile2.png +32 -o hyprball-c1.c1
+neogeoconvert scoreboard.png +1024 -o hyprball-c1.c1
+neogeoconvert ch1.png +1512 -o hyprball-c1.c1
+
+
+
